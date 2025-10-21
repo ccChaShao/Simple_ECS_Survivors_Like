920fe10ec324559c2e6af7ba8e3841b46de3d779
萝卜卜皮 <57029955+ccChaShao@users.noreply.github.com>
2025-10-21 15:20:37 +0800

    DIFF
    相机跟随

--- Assets/Scripts/ECS/PlayerAuthoring.cs
+++ Assets/Scripts/ECS/PlayerAuthoring.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace Charasiew.ECS
@@ -38,7 +39,42 @@ namespace Charasiew.ECS
     {
         public void OnCreate(ref SystemState state)
         {
-            
+            state.RequireForUpdate<InititalizeCameraTargetTag>();           // 当世界中存在至少一个具备指定组件的实体时，系统的OnUpdate才会执行；
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            if (CameraTargetSingleton.Instance == null)
+            {
+                return;
+            }
+
+            Transform cameraTargetTransform = CameraTargetSingleton.Instance.transform;
+
+            var ecb = new EntityCommandBuffer(state.WorldUpdateAllocator);
+            foreach (var (cameraTarget, entity) in SystemAPI.Query<RefRW<CameraTarget>>().WithAll<InititalizeCameraTargetTag, PlayerTag>().WithEntityAccess())
+            {
+                cameraTarget.ValueRW.cameraTransform = cameraTargetTransform;
+                ecb.RemoveComponent<InititalizeCameraTargetTag>(entity);            // 这里移除掉组件（结构性改变，所以使用ecb进行延迟性批量改动）
+            }
+
+            ecb.Playback(state.EntityManager);
+        }
+    }
+
+    /// <summary>
+    /// 相机移动系统
+    /// （放在transform系统组中更新）
+    /// </summary>
+    [UpdateAfter(typeof(TransformSystemGroup))]
+    public partial struct MoveCameraSystem : ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+            foreach (var (localToWorld, cameraTarget) in SystemAPI.Query<LocalToWorld, CameraTarget>().WithAll<PlayerTag>().WithNone<InititalizeCameraTargetTag>())         // 需要玩家标签，并且已经初始化完毕了
+            {
+                cameraTarget.cameraTransform.Value.position = localToWorld.Position;
+            }
         }
     }
     
