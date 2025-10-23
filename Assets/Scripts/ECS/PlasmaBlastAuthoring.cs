fe43ebc038f000d46ea42dafda09f23e37ca30f1
萝卜卜皮 <57029955+ccChaShao@users.noreply.github.com>
2025-10-23 17:45:29 +0800

    DIFF

--- Assets/Scripts/ECS/PlasmaBlastAuthoring.cs
+++ Assets/Scripts/ECS/PlasmaBlastAuthoring.cs
@@ -1,4 +1,7 @@
+using Unity.Collections;
 using Unity.Entities;
+using Unity.Physics;
+using Unity.Transforms;
 using UnityEngine;
 
 namespace Charasiew.ECS
@@ -24,6 +27,50 @@ namespace Charasiew.ECS
                     attackDamge = authoring.attackDamge,
                 });
             }
+        }
+    }
+
+    public partial struct PlasmaBlastMoveSystem : ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+            var deltaTime = SystemAPI.Time.DeltaTime;
+            foreach (var (transform, plasmaBlastData) in SystemAPI.Query<RefRW<LocalTransform>, RefRO<PlasmaBlastData>>())
+            {
+                // 右侧为正方形，应该创建的时候算了以右侧为正方向的欧拉角；
+                transform.ValueRW.Position += transform.ValueRO.Right() * plasmaBlastData.ValueRO.moveSpeed * deltaTime;
+            }
+        }
+    }
+    
+    public partial struct PlasmaBlastAttackSystem : ISystem
+    {
+        
+    }
+
+    public struct PlasmaBlastAttackJob : ITriggerEventsJob
+    {
+        [ReadOnly] public ComponentLookup<PlasmaBlastData> plasmaBlastLookup;
+        [ReadOnly] public ComponentLookup<EnemyTag> enemyLookup;
+        public BufferLookup<DamgeThisFrame> damgeThisFrameLookup;
+        
+        public void Execute(TriggerEvent triggerEvent)
+        {
+            Entity plasmaBlastEntity;
+            Entity enemyEntity;
+
+            if (plasmaBlastLookup.HasComponent(triggerEvent.EntityA) && enemyLookup.HasComponent(triggerEvent.EntityB))
+            {
+                plasmaBlastEntity = triggerEvent.EntityA;
+                enemyEntity = triggerEvent.EntityB;
+            }
+            else if (plasmaBlastLookup.HasComponent(triggerEvent.EntityB) && enemyLookup.HasComponent(triggerEvent.EntityA))
+            {
+                plasmaBlastEntity = triggerEvent.EntityB;
+                enemyEntity = triggerEvent.EntityA;
+            }
+            else
+                return;
         }
     }
 }
